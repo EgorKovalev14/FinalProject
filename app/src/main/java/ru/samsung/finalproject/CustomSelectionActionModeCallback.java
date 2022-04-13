852bea0f7a21209505ba996c80e5a60c158452ba
KovalevEgor14 <olegnorilsk77@gmail.com>
2022-04-13 14:32:53 +0500

    логи+не плывет разметка

--- app/src/main/java/ru/samsung/finalproject/CustomSelectionActionModeCallback.java
+++ app/src/main/java/ru/samsung/finalproject/CustomSelectionActionModeCallback.java
@@ -1,5 +1,6 @@
 package ru.samsung.finalproject;
 
+import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -7,17 +8,27 @@ import android.view.MenuItem;
 public class CustomSelectionActionModeCallback implements ActionMode.Callback {
     @Override
     public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+        Log.d("ActionTag", "onCreateActionMode started");
+
         return true;
     }
 
     @Override
     public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+        Log.d("ActionTag", "onPrepareActionMode started");
         try{
+            Log.d("ActionTag", " try-catch started");
             MenuItem copyItem=menu.findItem(android.R.id.copy);
+            Log.d("ActionTag", " item is found");
             CharSequence title = copyItem.getTitle();
+            Log.d("ActionTag", "item title found");
             menu.clear();
+            Log.d("ActionTag", " menu cleared");
             menu.add(0,android.R.id.copy, 0, title);
+            Log.d("ActionTag", "item added, try-catch finished");
         }catch(Exception e){
+            Log.d("ActionTag", "что-то пошло не так"+e.getMessage());
+            e.printStackTrace();
 
         }
         return false;
